f81079594839252b0af82f6ec1875c65ce5008bc
bminella <brunominella@hotmail.com>
2022-06-02 19:19:49 -0300

    chore: adiciona retrofit e gson

--- app/src/main/java/com/bminella/othereyes/Model/OtherEyesModel.kt
+++ app/src/main/java/com/bminella/othereyes/Model/OtherEyesModel.kt
@@ -1,3 +1,9 @@
 package com.bminella.othereyes.Model
 
+data class UnsplashQueryResult(val total: Int, val total_pages: Int, val results:List<UnsplashImageModel>)
+
+data class UnsplashImageModel(val id: String, val urls:UnsplashImageUrls)
+
+data class UnsplashImageUrls(val small: String, val full: String,val raw: String,val thumb: String,val small_s3: String,val regular: String,)
+
 data class OtherEyesModel(val image: Int, val name: String)
